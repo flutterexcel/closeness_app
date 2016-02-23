dca08c2bf9f6b57fe1833ca76fc514094d580671
deepakexcel <deepak.excel2011@gmail.com>
2016-02-23 16:25:00 +0530

    after login barnch commit

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,6 +1,5 @@
 if (Meteor.isClient) {
-
-    angular.module('closeness', ['angular-meteor', 'ui.router','ionic'])
+    angular.module('closeness', ['angular-meteor', 'ui.router','ionic', 'facebookLoginService'])
             .run(function($ionicPlatform,$state) {
                 console.log('run');
             });
---
+++ mobile-config.js
@@ -0,0 +1,4 @@
+App.configurePlugin('com.phonegap.plugins.facebookconnect', {
+    APP_ID: '1705896529656807',
+    APP_NAME: 'closeness'
+});
